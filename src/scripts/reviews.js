e4179c672a660fe536e63d97d4965e64f3dd86c6
TVitalii <64230122+TVitalii@users.noreply.github.com>
2020-08-29 20:08:31 +0300

    поменял на слайдер на vue-flickity

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -1,54 +1,65 @@
-import Vue from "vue";
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
-import "swiper/swiper-bundle.css";
+import Vue from 'vue';
+import Flickity from 'vue-flickity';
 
 new Vue({
-  el: "#slider-component",
-  template: "#slider-container",
-  components: {
-    Swiper, SwiperSlide
-  },
-  data() {
-
-    return {
-      reviews: [],
-      sliderOptions: {
-        breakpoints: {
-          // when window width is >= 320px
-          '320': {
-            slidesPerView: 1,
-          },
-          // when window width is >= 480px
-          '480': {
-            slidesPerView: 2,
-          },
-        }
-      }
-    }
-  },
-  methods: {
-    requireImagesToArray(data) {
-      return data.map((item) => {
-        const requiredImage = require(`../images/content/${item.pic}`)
-          .default;
-        item.pic = requiredImage;
-        return item;
-      });
+    el: '#slider-component',
+    template: '#slider',
+    components: {
+        Flickity
     },
-    slide(direction) {
-      const slider = this.$refs["slider"].$swiper;
-      switch(direction) {
-        case "next" :
-          slider.slideNext()
-          break;
-        case "prev" :
-          slider.slidePrev()
-          break;
-      }
+    data() {
+        return {
+            reviews: [],
+            flickityOptions: {
+                groupCells: true,
+                prevNextButtons: false,
+                pageDots: false,
+            }
+        }
+    },
+    methods: {
+        requiredPhotos() {
+            this.reviews = this.reviews.map(review => {
+                review.photo = require(`../images/content/${review.photo}`).default;
+                return review;
+            })
+        },
+        next() {
+            this.$refs.flickity.next();
+        },
+          
+        previous() {
+            this.$refs.flickity.previous();
+        }
+    },
+    mounted() {
+
+        this.requiredPhotos();
+
+        var ref = this.$refs;
+        
+        ref.prevBtn.style.opacity = .2;
+        ref.prevBtn.style.cursor = 'initial';
+
+        this.$refs.flickity.on('settle', function(pos) {
+            if (pos === this.slides.length - 1) {
+                 ref.nextBtn.style.opacity = .2;
+                 ref.nextBtn.style.cursor = 'initial';
+            } else {
+                 ref.nextBtn.style.opacity = 1;
+                 ref.nextBtn.style.cursor = 'pointer';
+            }
+ 
+            if (pos === 0) {
+                 ref.prevBtn.style.opacity = .2;
+                 ref.prevBtn.style.cursor = 'initial';
+             } else {
+                 ref.prevBtn.style.opacity = 1;
+                 ref.prevBtn.style.cursor = 'pointer';
+             }
+         })
+    },
+    created() {
+        this.reviews = require("../data/reviews.json")
     }
-  },
-  created() {
-    const data = require("../data/reviews.json");
-    this.reviews = this.requireImagesToArray(data)
-  }
 });
