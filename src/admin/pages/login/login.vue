9e8a4b09e3c196e275a78ccf60ff00f7fd683cb2
TVitalii <64230122+TVitalii@users.noreply.github.com>
2020-09-07 00:02:24 +0300

    корректировка формы Логин

---
+++ src/admin/pages/login/login.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="login-page-component">
+    <div class="content">
+      <form class="form" @submit.prevent="handleSubmit">
+        <div class="form-title">Авторизация</div>
+        <div class="row">
+          <app-input
+            title="Логин"
+            v-model="user.name"
+            icon="user"
+            :errorMessage="validation.firstError('user.name')"
+          />
+        </div>
+        <div class="row">
+          <app-input
+            title="Пароль"
+            v-model="user.password"
+            icon="key"
+            type="password"
+            :errorMessage="validation.firstError('user.password')"
+          />
+        </div>
+        <div class="btn">
+          <app-button :disabled="isSubmitDisabled" typeAttr="submit" title="Отправить" />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import appInput from "../../components/input";
+import appButton from "../../components/button";
+import { Validator, mixin as ValidatorMixin } from "simple-vue-validator";
+import axios from "axios";
+
+export default {
+  mixins: [ValidatorMixin],
+  validators: {
+    "user.name": value => {
+      return Validator.value(value).required("Введите имя пользователя");
+    },
+    "user.password": value => {
+      return Validator.value(value).required("Введите пароль");
+    },
+  },
+  data: () => ({
+    user: {
+      name: "",
+      password
+    }
+    isSubmitDisabled: false,
+  }),
+  components: { appButton, appInput },
+  methods: {
+    handeleSubmit() {
+      this.$validate().then((isValid) => {
+        if (isValid === false) return;
+
+        this.isSubmitDisabled = true;
+       
+        axios.post("https://webdev-api.loftschool.com/", this.user).then(respons => {
+          const token = response.data.token;
+          localStorage/setItem("token", token);
+          axios.defaults.headers["Authorization"] = `Bearer ${token}`;
+          this.$router.replace('/')
+        })
+        .catch (error) console.log(error))
+        .finally {
+          this.isSubmitDisabled = false;
+        })
+      });
+    },
+  },
+};
+</script>
+
+<style lang="postcss" scoped src="./login.pcss"></style>
